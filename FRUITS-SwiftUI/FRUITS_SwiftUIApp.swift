47671a186f87311b47619dbf4872e912355348b4
Shivang Mishra <shivangm@exzeo.com>
2023-06-10 15:25:36 +0530

    Initial Commit

---
+++ FRUITS-SwiftUI/FRUITS_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FRUITS_SwiftUIApp.swift
+//  FRUITS-SwiftUI
+//
+//  Created by Shivang Mishra on 10/06/23.
+//
+
+import SwiftUI
+
+@main
+struct FRUITS_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
